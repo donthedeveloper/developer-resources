0bcc28f393b01e68f2081c4b47c093edbdd077c9
Don <donhansen347@gmail.com>
2018-05-12 20:33:08 -0500

    updated with plan for resources

--- test/resourceModel.spec.js
+++ test/resourceModel.spec.js
@@ -2,10 +2,44 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
 const expect = chai.expect;
-const {Category} = require('../server/models');
+const {Resource} = require('../server/models');
 
 describe('Resource Model', () => {
-    describe('Add a resoruce to the database', () => {
+    describe('Add a resource to the database', () => {
+        const testCategory = 'testCategory';
+
+        it('responds with the object that was created when successful', (done) => {
+            Resource.create({
+                name: testCategory,
+                description: 'test description',
+                url: 'https://donthedeveloper.tv'
+            })
+        });
+
+        it ('sends appropriate error response when name field is never sent in body', (done) => {
+
+        });
+
+        it ('sends appropriate error response when name field value is blank', (done) => {
+
+        });
+
+        it ('sends appropriate error response when name field is a duplicate entry', (done) => {
+
+        });
+    });
+
+    describe('Can get a list of all resources', (done) => {
+        it('responds with an array', (done) => {
+
+        });
+
+        it('contains resource objects inside that array', (done) => {
+
+        });
+    });
+
+    describe('Can update resource', (done) => {
 
     });
 });
